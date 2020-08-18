02b1d953514d79eec5dca460ef62e5925df52751
Peter Cai <peter@typeblog.net>
2020-08-18 17:16:00 +0800

    loop: only use as much space of poll_fds as needed
    * if we always keep poll_fds_len at maximum, the performance of poll()
      would suffer since it needs to look over a lot of invalid fds for no
      reason every single time.

--- loop.c
+++ loop.c
@@ -4,6 +4,10 @@
 // List of all fds that we need to poll
 struct pollfd *poll_fds;
 size_t poll_fds_len = 0;
+// Reverse map from fd number to the index in poll_fds
+// Make sure the length of this is always equal to or larger than RLIMIT_NOFILE
+// Otherwise it may not be able to house all possible fds
+size_t *poll_fds_reverse_map;
 // Hooks
 loop_before_poll_hook_t hooks_before_poll[NUM_HOOKS_MAX];
 size_t hooks_before_poll_num = 0;
@@ -11,14 +15,14 @@ loop_after_poll_hook_t hooks_after_poll[NUM_HOOKS_MAX];
 size_t hooks_after_poll_num = 0;
 
 int event_loop_init(size_t max_fds) {
-    poll_fds_len = max_fds;
     // Allocate the maximum possible amount of memory
     // max_fds is normally in the magnitude of thousands
     // or tens of thousands, so the memory consumption
     // should be fine
     poll_fds = malloc(max_fds * sizeof(struct pollfd));
+    poll_fds_reverse_map = malloc(max_fds * sizeof(size_t));
 
-    if (poll_fds == NULL) {
+    if (poll_fds == NULL || poll_fds_reverse_map == NULL) {
         return -1;
     }
 
@@ -26,31 +30,48 @@ int event_loop_init(size_t max_fds) {
 
     for (int i = 0; i < max_fds; i++) {
         poll_fds[i].fd = -1;
+        poll_fds_reverse_map[i] = -1;
     }
 }
 
 void event_loop_add_fd(int fd, short events) {
-    poll_fds[fd].fd = fd;
-    poll_fds[fd].events = events;
-    poll_fds[fd].revents = 0;
+    // Find an empty slot to add the fd
+    size_t slot = 0;
+    // <= is intentional: poll_fds[poll_fds_len] will
+    // always be empty. This is needed when no empty
+    // slots are available before the tail
+    for (slot = 0; slot <= poll_fds_len; slot++) {
+        if (poll_fds[slot].fd == -1) break;
+    }
+
+    poll_fds[slot].fd = fd;
+    poll_fds[slot].events = events;
+    poll_fds[slot].revents = 0;
+    poll_fds_reverse_map[fd] = slot;
+
+    if (slot == poll_fds_len) {
+        poll_fds_len++;
+    }
 }
 
 void event_loop_remove_fd(int fd) {
-    poll_fds[fd].fd = -1;
-    poll_fds[fd].events = 0;
-    poll_fds[fd].revents = 0;
+    int idx = poll_fds_reverse_map[fd];
+    poll_fds[idx].fd = -1;
+    poll_fds[idx].events = 0;
+    poll_fds[idx].revents = 0;
+    poll_fds_reverse_map[fd] = -1;
 }
 
 void event_loop_set_fd_events(int fd, short events) {
-    poll_fds[fd].events |= events;
+    poll_fds[poll_fds_reverse_map[fd]].events |= events;
 }
 
 void event_loop_clear_fd_events(int fd, short events) {
-    poll_fds[fd].events &= ~events;
+    poll_fds[poll_fds_reverse_map[fd]].events &= ~events;
 }
 
 int event_loop_get_fd_revents(int fd) {
-    return poll_fds[fd].revents;
+    return poll_fds[poll_fds_reverse_map[fd]].revents;
 }
 
 // We assume we never register more than NUM_HOOKS_MAX
