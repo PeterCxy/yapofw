f609c04e2ae8ee63056c54b4daf04d1f152a6525
Peter Cai <peter@typeblog.net>
2020-08-19 19:22:15 +0800

    tcp: add comments about tcp_sock_session_t

--- tcp.h
+++ tcp.h
@@ -10,21 +10,42 @@ typedef struct {
 } tcp_sock_listen_t;
 
 typedef struct tcp_sock_session_t {
+    // The index of the corresponding config item of this session
     size_t cfg_idx;
+    // 1 if this session has never yet been put into a poll()
     int new_connection;
+    // The fd from the client to yapofw
     int incoming_fd;
+    // The fd from yapofw to the target host
     int outgoing_fd;
+    // Buffer for the incoming-outgoing direction
     char incoming_outgoing_buf[BUF_SIZE];
+    // Length of the used portion of the buffer
     int incoming_outgoing_buf_len;
+    // Length of the portion of the buffer that
+    // has been written (within incoming_outgoing_buf_len)
+    // This is for handling the rare case when write()
+    // returns less than the buffer length
     int incoming_outgoing_buf_written;
+    // Buffer for the outgoing-incoming direction
     char outgoing_incoming_buf[BUF_SIZE];
+    // See the incoming counterpart
     int outgoing_incoming_buf_len;
+    // See the incoming counterpart
     int outgoing_incoming_buf_written;
+    // 1 if the incoming-outgoing direction has errored
+    // or has been shut down
     int incoming_outgoing_shutdown;
+    // 1 if the outgoing-incoming direction has errored
+    // or has been shut down
     int outgoing_incoming_shutdown;
+    // Address of the client
     struct sockaddr client_addr;
+    // Address of the target host
     struct sockaddr dst_addr;
+    // The previous session in the linked list
     struct tcp_sock_session_t *prev_session;
+    // The next session in the linked list
     struct tcp_sock_session_t *next_session;
 } tcp_sock_session_t;
 
